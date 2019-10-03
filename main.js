9827a55d85618176c4b2e592b32c8075c88f27e5
Alejo <alejandrofranco@pursuit.org>
2019-10-03 12:09:18 -0400

    load file from arg passed in command line

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const fs = require('fs');
 
+const WORDS = '-w'
+const SYMBOLS = '-s'
+
 // Write a JS program that finds the most commonly used words in your code i.e 'let', 'const', 'true'
 // and the most commonly used symbols i.e '=', '&', '$'
 
-fs.readFile('sample.js', 'utf8', (err, data) => {
-  let words = data.split(/\W+/);
-  const count = getWordsCount(words)
-  console.log(count);
-})
+
+
+const countWordsInFile = (file) => {
+  fs.readFile(file, 'utf8', (err, data) => {
+    if (err) throw err;
+    const words = data.split(/\W+/);
+    const count = getWordsCount(words)
+    console.log(count)
+  })
+}
 
 const getWordsCount = (words) => {
   let counter = {}
@@ -30,3 +38,21 @@ const getWordsCount = (words) => {
   const sortedByCount = counts.sort((a, b) => b.count - a.count);
   return sortedByCount;
 }
+
+const main = () => {
+  const args = process.argv.slice(2)
+  if (args.length > 2) {
+    throw new Error('Too many arguments specified');
+  }
+
+  const targetFile = args[1];
+  const option = args[0];
+
+  switch (option) {
+    case WORDS:
+      countWordsInFile(targetFile);
+      break;
+  }
+}
+
+main();
